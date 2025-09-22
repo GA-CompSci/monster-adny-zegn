e8cc0ebb515a927e3f1e9d41e6f11e6c83cb1613
adny-zegn <143519714+adny-zegn@users.noreply.github.com>
2025-09-22 10:36:22 -0400

    idk how to avoid the error at the beginning

--- src/App.java
+++ src/App.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        private static Monster[] monsterParty;
+
+        System.out.println("----- THE MONSTER PARTY BEGINS -----");
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many monsters shall you slay? Speak: ");
+
+        int num = input.nextInt(); // TODO: only allow positive ints
+        monsterParty = new Monster[num];
+        // invite all monsters to the party
+        for (int i = 0; i < monsterParty.length; i++) {
+            monsterParty[i] = new Monster(); // TODO: create some neat specials
+        }
+
+        // HOW MANY MONSTERS HAVE >50 HEALTH???
+        System.out.println(monsterCount(50, monsterParty) + " monsters have more than 50 hp! Egad!");
+
+    }
+
+    /**
+     * Returns how many monsters have MORE than the given health.
+     * @param health The health threshold to check.
+     * @param monsterFamily The monster array to check.
+     * @return The number of monsters with hp above the given.
+     */
+    public static int monsterCount(int health, Monster[] monsterFamily) {
+        int count = 0;
+        for (Monster monster : monsterFamily) {
+            if (monster.health() > health) count++;
+        }
+        return count;
+    }
+
+    /**
+     * Returns the next monster of the monster party.
+     * @return 
+     */
+    public static Monster getNextMonster() {
+        for (int i = 0; i < monsterParty.length; i++) {
+
+        }
     }
 }
---
+++ src/Monster.java
@@ -0,0 +1,31 @@
+public class Monster {
+    // INSTANCE VARIABLES - PROPERTIES - ATTRIBUTES
+    private int health;
+    private double damage;
+    private int speed;
+    private String special;
+
+    // CONSTRUCTOR
+    public Monster() {
+        // RANDOM ATTRIBUTE GENERATION
+        health = (int)(Math.random() * 81) + 20;
+        // damage: random 10-50
+        damage = (Math.random() * 41) + 10;
+        // speed: random 1-10
+        speed = (int)(Math.random() * 9) + 1;
+        // no special by default
+        special = "";
+    }
+    // OVERLOADED CONSTRUCTOR
+    public Monster(String special) {
+        this();
+        this.special = special;
+    }
+
+    // ACCESSOR METHODS
+    public int health() {return this.health;}
+    public double damage() {return this.damage;}
+    public int speed() {return this.speed;}
+    public String special() {return this.special;}
+    
+}
